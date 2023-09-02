9ed3018f92482109b8120f7ec4dc40ba0888513b
veera sekhar <115022648+VEERASEKHAR-GIT@users.noreply.github.com>
2023-09-02 18:14:52 -0500

    Categories table

--- database_setup.sql
+++ database_setup.sql
@@ -2,6 +2,10 @@
 # Establish a connection to the SQLite database
 conn = sqlite3.connect("merch_store.db")
 cursor = conn.cursor()
+# Sample data for Categories table
+cursor.execute("INSERT INTO Categories (name, description) VALUES (?, ?)", ("Electronics", "Electronic gadgets and accessories"))
+cursor.execute("INSERT INTO Categories (name, description) VALUES (?, ?)", ("Clothing", "Apparel for all occasions"))
+cursor.execute("INSERT INTO Categories (name, description) VALUES (?, ?)", ("Books", "A wide selection of books"))
 
 
 # Commit changes and close the connection
