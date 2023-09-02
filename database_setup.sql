161913d2bde03df43ba90a3dc3c97759714c7376
veera sekhar <115022648+VEERASEKHAR-GIT@users.noreply.github.com>
2023-09-02 18:14:07 -0500

    Update database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -1,1 +1,9 @@
 
+# Establish a connection to the SQLite database
+conn = sqlite3.connect("merch_store.db")
+cursor = conn.cursor()
+
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
