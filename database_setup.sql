69b683a023b6fbbf177c09fb142478a40f13ea79
veera sekhar <115022648+VEERASEKHAR-GIT@users.noreply.github.com>
2023-09-02 18:15:15 -0500

    Update database_setup.sql
    Sample data for Products table

--- database_setup.sql
+++ database_setup.sql
@@ -6,6 +6,10 @@ cursor = conn.cursor()
 cursor.execute("INSERT INTO Categories (name, description) VALUES (?, ?)", ("Electronics", "Electronic gadgets and accessories"))
 cursor.execute("INSERT INTO Categories (name, description) VALUES (?, ?)", ("Clothing", "Apparel for all occasions"))
 cursor.execute("INSERT INTO Categories (name, description) VALUES (?, ?)", ("Books", "A wide selection of books"))
+# Sample data for Products table
+cursor.execute("INSERT INTO Products (name, description, price, stock_quantity, category_id) VALUES (?, ?, ?, ?, ?)", ("Laptop", "High-performance laptop", 999.99, 50, 1))
+cursor.execute("INSERT INTO Products (name, description, price, stock_quantity, category_id) VALUES (?, ?, ?, ?, ?)", ("T-Shirt", "Comfortable cotton t-shirt", 19.99, 100, 2))
+cursor.execute("INSERT INTO Products (name, description, price, stock_quantity, category_id) VALUES (?, ?, ?, ?, ?)", ("Sci-Fi Novel", "Bestselling science fiction book", 14.99, 75, 3))
 
 
 # Commit changes and close the connection
